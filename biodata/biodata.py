8990939d5d22e96da7b502ca845fdf2aa07b6900
Muhammad Wildan <wildanrgssport@gmail.com>
2018-10-20 10:27:08 +0700

    Create biodata.py

---
+++ biodata/biodata.py
@@ -0,0 +1,7 @@
+def biodata():
+    nama = "Nama : wildan\n" #ganti nama dengan namamu
+    asal = "Asal : gresik\n" #ganti nama dengan namamu
+    id_telegram = "Id Telegram : gelembungsahabat" #ganti nama dengan namamu
+    print(nama + asal + id_telegram) #munculin ke terminal
+
+biodata()
